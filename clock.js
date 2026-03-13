e2adaa699e14a75fc6f091e5338cdf9588c627d6
Luur <argoluur@tlu.ee>
2026-03-13 23:48:29 +0200

    Kodutöö lisamine

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
 console.log("fail ühendatud");
 const pi = 3.14;
 let hours, minutes, seconds, day, month, year, dateTime;
-let fontSize = 25;
+let fontSize = 100;
 
 function changeFontSizeBigger(){
     fontSize = fontSize + 5;
@@ -21,6 +21,42 @@ function changeFontSizeSmaller(){
     }
     document.getElementById('dateContainer').style.fontSize =  fontSize + "px";
     document.getElementById('clockContainer').style.fontSize =  fontSize + "px";
+}
+
+function randomizeBackgroundColor(){
+    min = 0;
+    max = 255;
+    // random valemi leidsin googeldades Math.random()
+    r = Math.floor(Math.random() * (max - min + 1)) + min;
+    g = Math.floor(Math.random() * (max - min + 1)) + min;
+    b = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    document.getElementById('clockBody').style.backgroundColor = "rgb(" + r +","+ g + "," + b +")";
+}
+
+function changeClockOrder(){
+    order = document.getElementById('container').style.flexDirection;
+    if(order == "column"){
+        document.getElementById('container').style.flexDirection = "column-reverse";
+        console.log("reverse")
+    } else {
+        document.getElementById('container').style.flexDirection = "column";
+        console.log("column")
+    }
+}
+
+function moveClock(){
+    direction = this.id;
+    if(direction == "moveLeft"){
+        console.log("left");
+        document.getElementById('container').style.alignItems ="flex-start";
+    } else if (direction == "moveRight"){
+        console.log("right");
+        document.getElementById('container').style.alignItems ="flex-end";
+    } else {
+        console.log("center");
+        document.getElementById('container').style.alignItems ="center";
+    }
 }
 
 function upDateClock() {
@@ -59,7 +95,7 @@ function updateDate(){
     }
 
     document.getElementById('day').innerHTML = day + ".";
-    document.getElementById('month').innerHTML = month + ":";
+    document.getElementById('month').innerHTML = month + ".";
     document.getElementById('year').innerHTML = year;
 }
 
@@ -73,10 +109,35 @@ function checkKey(e){
     }
 }
 
+function rainbowClock(){
+    
+    if(document.getElementById('clockContainer').style.animationName == "none"){
+        document.getElementById('clockContainer').style.animationName = "rainbowText";
+        document.getElementById('dateContainer').style.animationName = "rainbowText";
+        
+    } else {
+        document.getElementById('clockContainer').style.animationName = "none";
+        document.getElementById('dateContainer').style.animationName = "none";
+    }
+}
+
+
+
 upDateClock();
 updateDate();
 setInterval(upDateClock, 1000);
 setInterval(updateDate, 60000);
 document.getElementById('bigger').addEventListener('click', changeFontSizeBigger);
 document.getElementById('smaller').addEventListener('click', changeFontSizeSmaller);
+
+document.getElementById('randomColor').addEventListener('click', randomizeBackgroundColor);
+
 window.addEventListener('keypress', checkKey);
+
+document.getElementById('changeClockOrder').addEventListener('click', changeClockOrder);
+
+document.getElementById('moveLeft').addEventListener('click', moveClock);
+document.getElementById('moveCenter').addEventListener('click', moveClock);
+document.getElementById('moveRight').addEventListener('click', moveClock);
+
+document.getElementById('rainbowText').addEventListener('click', rainbowClock);
